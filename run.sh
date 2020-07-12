0bca421aca4459441b982db239fa27e02fac79e5
Alexey Kirpichnikov <alex.kirp@gmail.com>
2020-07-12 10:37:39 +0500

    Log request error codes

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -e
-
 SERVER_URL=$1
 PLAYER_KEY=$2
 
 echo "ServerUrl: $SERVER_URL; PlayerKey: $PLAYER_KEY"
-curl -s -XPOST -d "$PLAYER_KEY" -s "$SERVER_URL"
+RESPONSE=$(curl -s -XPOST -d "$PLAYER_KEY" -s "$SERVER_URL")
+EXIT_CODE=$?
+echo "Server response: $RESPONSE"
+if [ $EXIT_CODE != "0" ]; then echo "run error code: $EXIT_CODE"; fi
