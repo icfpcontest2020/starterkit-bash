5c36c3ea614af9bf82ff3f565d8ce364317b9122
Alexey Kirpichnikov <alex.kirp@gmail.com>
2020-07-12 12:23:29 +0500

    Better error processing

--- run.sh
+++ run.sh
@@ -4,7 +4,21 @@ SERVER_URL=$1
 PLAYER_KEY=$2
 
 echo "ServerUrl: $SERVER_URL; PlayerKey: $PLAYER_KEY"
-RESPONSE=$(curl -s -XPOST -d "$PLAYER_KEY" -s "$SERVER_URL")
+
+RESPONSE_FILE=$( mktemp -t tmp.XXXXXXXXXX )
+HTTP_CODE=$( curl -s -o "$RESPONSE_FILE" -w '%{http_code}' -XPOST -d "$PLAYER_KEY" "$SERVER_URL" )
 EXIT_CODE=$?
-echo "Server response: $RESPONSE"
-if [ $EXIT_CODE != "0" ]; then echo "run error code: $EXIT_CODE"; fi
+
+if [[ ${EXIT_CODE} -ne 0 ]] ; then
+    echo "run error code: $EXIT_CODE"
+    exit 0
+fi
+
+if [[ ${HTTP_CODE} -ne 200 ]] ; then
+    echo "Unexpected server response:"
+    echo "HTTP code: $HTTP_CODE"
+    echo "Response body: $( cat "$RESPONSE_FILE" )"
+    exit 0
+fi
+
+echo "Server response: $( cat "$RESPONSE_FILE" )"
